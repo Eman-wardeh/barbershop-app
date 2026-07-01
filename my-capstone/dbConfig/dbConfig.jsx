696d821b4cd2d77bebd6a0a0b4919f420b653576
Eman-wardeh <emwardeh1995@hotmail.com>
2026-07-01 17:38:52 -0500

    update dbConfig

--- my-capstone/dbConfig/dbConfig.jsx
+++ my-capstone/dbConfig/dbConfig.jsx
@@ -1,18 +1,33 @@
+// import mongoose from "mongoose";
+
+// export async function connectToDb(){
+//     try{
+//         await mongoose.connect(process.env.DB_URI); 
+//         const connection = mongoose.connection;
+//         connection.on('connected', ()=>{
+//             console.log("Connected to the database successfully!");
+//         })
+//         connection.on('error', (err) =>{
+//             console.error("Database connection error:", err);
+//             process.exit(1); // Exit the process with an error code
+//         })
+//     }
+//     catch(error){
+//         console.error("Error connecting to the database:", error);
+//     }
+// }
 import mongoose from "mongoose";
 
-export async function connectToDb(){
-    try{
-        await mongoose.connect(process.env.DB_URI); 
-        const connection = mongoose.connection;
-        connection.on('connected', ()=>{
-            console.log("Connected to the database successfully!");
-        })
-        connection.on('error', (err) =>{
-            console.error("Database connection error:", err);
-            process.exit(1); // Exit the process with an error code
-        })
-    }
-    catch(error){
-        console.error("Error connecting to the database:", error);
+export async function connectToDb() {
+    try {
+        if (mongoose.connection.readyState >= 1) {
+            return;
+        }
+
+        await mongoose.connect(process.env.DB_URI);
+
+        console.log("Connected to the database successfully!");
+    } catch (error) {
+        console.error("Database connection error:", error);
     }
 }
