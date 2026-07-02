415edf8dbe5be46bb735029bd64ed2bea3002079
Eman-wardeh <emwardeh1995@hotmail.com>
2026-07-02 11:23:31 -0500

    update middleware2

--- my-capstone/middleware.js
+++ my-capstone/middleware.js
@@ -1,33 +1,28 @@
+// 
 import { NextResponse } from 'next/server'
- 
+
 export function middleware(req) {
-    //create public path that allows user who is not signed up,
-    //  or logged in to see only /login, /signup
+    const path = req.nextUrl.pathname;
 
-    const path = req.nextUrl.pathname;  // <= how to get the current path
-    const isPublicPath = 
-        path === '/login' || path === '/signup' || path === '/verifyemail';
+    const isPublicPath =
+        path === '/login' ||
+        path === '/signup' ||
+        path === '/verifyemail';
 
-    const token = req.cookies.get('jwt')?.value || '';
-    // If user is logged in and tries to access login/signup
-    if(isPublicPath && token){
-        return NextResponse.redirect(new URL('/', req.nextUrl))
+    const token = req.cookies.get('jwt')?.value;
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', req.url));
     }
 
-    // If user is NOT logged in and tries to access protected pages
-    if(!isPublicPath && !token){
-        return NextResponse.redirect(new URL('/login', req.nextUrl))
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', req.url));
     }
+
+    return NextResponse.next();
 }
-//see matching paths below to learn more
 export const config = {
     matcher: [
-        "/admin/:path*",
-        "/barber/:path*",
-        "/dashboard/:path*",
-        "/barberWorkingHours",
-        "/barberTimeOff",
-
-
+        "/((?!_next/static|_next/image|favicon.ico).*)",
     ]
 }
