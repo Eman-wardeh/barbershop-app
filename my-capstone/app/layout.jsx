1e3ddba33cff0c72fac56f3718145397d9aef0da
Eman-wardeh <emwardeh1995@hotmail.com>
2026-07-02 12:41:44 -0500

    update 1

--- my-capstone/app/layout.jsx
+++ my-capstone/app/layout.jsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Iron Cuts | Modern Barbershop",
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" >
       <body>
-        <Navbar />
+        {/* <Navbar /> */}
         <main className="page-content">
           {children}
         </main>
