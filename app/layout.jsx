8ecab5713d06878bb4dbbe88fc8268026f13ebac
Eman-wardeh <emwardeh1995@hotmail.com>
2026-07-05 08:26:46 -0500

    update background fix21

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <footer>
-        <p style={{color:"red"}}>© IRON CUTS. All Rights Reserved.</p>
+        <p>© IRON CUTS. All Rights Reserved.</p>
       </footer>
       </body>
       
